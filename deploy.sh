0b30733b1f5af5ed6fecd36dbc5f2fa227af62a3
KristiyanShahinyan <132195251+KristiyanShahinyan@users.noreply.github.com>
2023-05-13 14:16:27 +0000

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git pull
+tag=$(git describe --tags --abbrev=0)
+echo "Deploying version: ${tag}"
+sed -i.bak -r "s/version: '.*?'/version: '${tag}'/g" serverless.yaml
+serverless deploy
